c660a81e9779836b08b13ddd66c617187ac8e141
arielmell0 <arielmello@rede.ulbra.br>
2022-11-18 12:00:04 -0300

    feat: loading

--- components/Custom/PokemonCard.js
+++ components/Custom/PokemonCard.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "../primitives/Card";
+import loading from '../../assets/svg/loading.svg'
+
 const PokemonCard = (props) => {
   const [listPokemons, setListPokemons] = useState([])
   const [loading, setLoading] = useState(true)
@@ -24,7 +26,7 @@ const PokemonCard = (props) => {
   return (
     <>
       { loading ?
-        <div>'loading'</div> :
+        <div>{loading}</div> :
         <Card text={listPokemons.name.charAt(0).toUpperCase() + listPokemons.name.slice(1)} img={listPokemons.sprites.front_default}/>
       }
     </>
